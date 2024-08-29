f72c502e37fb88fc6b579781d0c3b4d938cd8e83
haris-yousaf <harisy260@gmail.com>
2024-08-29 18:02:27 +0500

    Invoice

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,16 +1,19 @@
 export default function Nav() {
 
     return (
-        <nav className="flex items-center justify-between h-3 w-full py-6 px-28 bg-white">
-            <div className="flex items-center justify-between">
-                <a className="text-xl font-semibold" href="/">Invoice-Generator</a>
+        <nav className="flex items-center justify-between h-3 w-full py-6 px-5 lg:px-20 xl:px-28 bg-white">
+            <a className="text-xl font-semibold" href="/">Invoice-Generator</a>
+            <div className="lg:flex items-center justify-between hidden">
                 <a className="ml-16 text-sm text-gray-500" href="/">Help</a>
                 <a className="ml-14 text-sm text-gray-500" href="/">History</a>
                 <a className="ml-14 text-sm text-gray-500" href="/">Invoicing Guide</a>
             </div>
-            <div className="">
-                <a className="mr-10 text-sm text-gray-500" href="./signin">Sign In</a>
-                <a className="mr-10 text-sm text-white border-hidden bg-[#009e74] px-4 py-2.5 rounded" href="./signup">Sign Up</a>
+            <div className="hidden sm:block">
+                <a className="mr-7 text-sm text-gray-500" href="./signin">Sign In</a>
+                <a className="text-sm text-white border-hidden bg-[#009e74] px-4 py-2.5 rounded" href="./signup">Sign Up</a>
+            </div>
+            <div className="sm:hidden">
+                <a href="#">&#8801;</a>
             </div>
         </nav>
     )
--- app/main/page.tsx
+++ app/main/page.tsx
@@ -7,11 +7,11 @@ export default function Main() {
             <Navbar/>
             <div className="w-full p-32">
                 <div className="w-full min-h-52 bg-white py-6 rounded-2xl drop-shadow-lg shadow-slate-800">
-                    <div className="flex text-left justify-between box-border px-8">
+                    <div className="flex flex-col text-left justify-between box-border px-8">
                         <div className="w-[80%]">
                             <h1 className="text-2xl font-semibold">My Invoices</h1>
                         </div>
-                        <div className="w-[20%] text-right">
+                        <div className="w-[20%] flex text-right">
                             <a href="./create" className="bg-[#009e74] py-3 text-sm px-6 text-white rounded-md rounded-r-none hover:bg-green-700">New Invoice</a>
                             <select className="w-10 bg-[#009e74] outline-none py-[9.8px] text-white rounded-md rounded-l-none hover:bg-green-700 hover:cursor-pointer">
                                 <option></option>
