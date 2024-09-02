8d3ccc8e77afa9725565ae1b7ff06c00a8a61f24
haris-yousaf <harisy260@gmail.com>
2024-09-02 14:28:18 +0500

    Invoice

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -4,14 +4,14 @@ import Navbar from "../components/navAfter";
 export default function Main() {
     return (
         <>
-            <Navbar/>
+            <Navbar />
             <div className="w-full p-32">
                 <div className="w-full min-h-52 bg-white py-6 rounded-2xl drop-shadow-lg shadow-slate-800">
-                    <div className="flex flex-col text-left justify-between box-border px-8">
-                        <div className="w-[80%]">
+                    <div className="w-full flex flex-col gap-2 md:flex-row text-left align-middle justify-between box-border px-8">
+                        <div className="w-full">
                             <h1 className="text-2xl font-semibold">My Invoices</h1>
                         </div>
-                        <div className="w-[20%] flex text-right">
+                        <div className="w-full flex md:justify-end">
                             <a href="./create" className="bg-[#009e74] py-3 text-sm px-6 text-white rounded-md rounded-r-none hover:bg-green-700">New Invoice</a>
                             <select className="w-10 bg-[#009e74] outline-none py-[9.8px] text-white rounded-md rounded-l-none hover:bg-green-700 hover:cursor-pointer">
                                 <option></option>
